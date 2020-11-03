75dccfce6bf2e451e134ed24d1e2a4262207d574
Arslanoov <rasularslanoov@gmail.com>
2020-11-03 15:24:24 +0500

    Added front controller

--- furious/public/index.php
+++ furious/public/index.php
@@ -2,4 +2,31 @@
 
 declare(strict_types=1);
 
-echo '2';
+use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Nyholm\Psr7Server\ServerRequestCreator;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+define('ENV', 'dev');
+
+$psr17Factory = new Psr17Factory();
+
+$creator = new ServerRequestCreator(
+    $psr17Factory, // ServerRequestFactory
+    $psr17Factory, // UriFactory
+    $psr17Factory, // UploadedFileFactory
+    $psr17Factory  // StreamFactory
+);
+
+$serverRequest = $creator->fromGlobals();
+
+$responseBody = $psr17Factory->createStream('Furious 2.0');
+$response = $psr17Factory->createResponse(200)
+    ->withBody($responseBody);
+
+if (ENV === 'dev') {
+    $response = $response->withHeader('X-Developer', 'Arslanoov');
+}
+
+(new SapiEmitter())->emit($response);
